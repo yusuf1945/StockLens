7bf6a9e3d0292c1be20398845693ae31ab252ea4
Yusuf <yusufhaque2020@gmail.com>
2025-11-04 13:33:24 +0530

    last commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true,s
+    ignoreBuildErrors: true,
   },
 };
 
